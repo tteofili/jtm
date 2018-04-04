9cd39b24a5581d97f410f0a0eeeaa36720ec71c8
Simo Tripodi <stripodi@adobe.com>
2018-04-04 15:32:56 +0200

    project split in multi-modules

--- core/src/main/java/com/github/tteofili/jtm/JiraAnalysisTool.java
+++ core/src/main/java/com/github/tteofili/jtm/JiraAnalysisTool.java
@@ -36,8 +36,6 @@ import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.xpack.client.PreBuiltXPackTransportClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import picocli.CommandLine.IVersionProvider;
-
 /**
  * Tool for analyzing Jira issues exported as XML.
  *
@@ -148,126 +146,6 @@ public class JiraAnalysisTool {
         client.close();
       }
     }
-  }
-
-
-  public static final class JtmVersionProvider implements IVersionProvider {
-
-    @Override
-    public String[] getVersion() throws Exception {
-      return new String[] {
-          "                         ''~``",
-          "                        ( o o )",
-          "+------------------.oooO--(_)--Oooo.------------------+",
-          String.format("%s v%s (built on %s)",
-              System.getProperty("project.artifactId"),
-              System.getProperty("project.version"),
-              System.getProperty("build.timestamp")),
-          String.format("Java version: %s, vendor: %s",
-              System.getProperty("java.version"),
-              System.getProperty("java.vendor")),
-          String.format("Java home: %s", System.getProperty("java.home")),
-          String.format("Default locale: %s_%s, platform encoding: %s",
-              System.getProperty("user.language"),
-              System.getProperty("user.country"),
-              System.getProperty("sun.jnu.encoding")),
-          String.format("OS name: \"%s\", version: \"%s\", arch: \"%s\", family: \"%s\"",
-              System.getProperty("os.name"),
-              System.getProperty("os.version"),
-              System.getProperty("os.arch"),
-              getOsFamily()),
-          "                     .oooO                            ",
-          "                     (   )   Oooo.                    ",
-          "+---------------------\\ (----(   )--------------------+",
-          "                       \\_)    ) /",
-          "                             (_/"
-      };
-    }
-
-    private static final String getOsFamily() {
-      String osName = System.getProperty("os.name").toLowerCase();
-      String pathSep = System.getProperty("path.separator");
-
-      if (osName.indexOf("windows") != -1) {
-        return "windows";
-      } else if (osName.indexOf("os/2") != -1) {
-        return "os/2";
-      } else if (osName.indexOf("z/os") != -1 || osName.indexOf("os/390") != -1) {
-        return "z/os";
-      } else if (osName.indexOf("os/400") != -1) {
-        return "os/400";
-      } else if (pathSep.equals(";")) {
-        return "dos";
-      } else if (osName.indexOf("mac") != -1) {
-        if (osName.endsWith("x")) {
-          return "mac"; // MACOSX
-        }
-        return "unix";
-      } else if (osName.indexOf("nonstop_kernel") != -1) {
-        return "tandem";
-      } else if (osName.indexOf("openvms") != -1) {
-        return "openvms";
-      } else if (pathSep.equals(":")) {
-        return "unix";
-      }
-
-      return "undefined";
-    }
-
-  }
-
-  private static final class ShutDownHook extends Thread {
-
-    private final long start = System.currentTimeMillis();
-
-    public ShutDownHook() {
-      super("shutdown-hook");
-    }
-
-    public void run() {
-      // format the uptime string
-
-      Formatter uptime = new Formatter();
-      uptime.format("Total uptime:");
-
-      long uptimeInSeconds = (System.currentTimeMillis() - start) / 1000;
-      final long hours = uptimeInSeconds / 3600;
-
-      if (hours > 0) {
-        uptime.format(" %s hour%s", hours, (hours > 1 ? "s" : ""));
-      }
-
-      uptimeInSeconds = uptimeInSeconds - (hours * 3600);
-      final long minutes = uptimeInSeconds / 60;
-
-      if (minutes > 0) {
-        uptime.format(" %s minute%s", minutes, (minutes > 1 ? "s" : ""));
-      }
-
-      uptimeInSeconds = uptimeInSeconds - (minutes * 60);
-
-      if (uptimeInSeconds > 0) {
-        uptime.format(" %s second%s", uptimeInSeconds, (uptimeInSeconds > 1 ? "s" : ""));
-      }
-
-      log.info(uptime.toString());
-      uptime.close();
-
-      log.info("Finished at: {}", new Date());
-
-      final Runtime runtime = Runtime.getRuntime();
-      final int megaUnit = 1024 * 1024;
-      log.info("Final Memory: {}M/{}M",
-          (runtime.totalMemory() - runtime.freeMemory()) / megaUnit,
-          runtime.totalMemory() / megaUnit);
-
-      log.info("                     .oooO                            ");
-      log.info("                     (   )   Oooo.                    ");
-      log.info("+---------------------\\ (----(   )--------------------+");
-      log.info("                       \\_)    ) /");
-      log.info("                             (_/");
-    }
-
   }
 
 }
